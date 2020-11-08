c7c3a040c94d81397bfef0acb80f3004c3b3b04b
ocakdans <selim.ocakdan@gmail.com>
2020-11-08 20:08:02 +0300

    4th commit from local master branch

--- src/firtstpackage/FirstClass.java
+++ src/firtstpackage/FirstClass.java
@@ -9,6 +9,7 @@ public class FirstClass {
 		System.out.println("first  commit from remotemaster branch");
 		System.out.println("second commit from remotemaster branch");
 		System.out.println("third  commit from remotemaster branch");
+		System.out.println("Here is a merge conflict I create from local master");
 
 	}
 
