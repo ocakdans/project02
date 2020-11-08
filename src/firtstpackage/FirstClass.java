960c860307d7f121bad1fc2872aac7078edd50a5
ocakdans <selim.ocakdan@gmail.com>
2020-11-08 20:34:49 +0300

    6th commit from local

--- src/firtstpackage/FirstClass.java
+++ src/firtstpackage/FirstClass.java
@@ -13,6 +13,7 @@ public class FirstClass {
 		System.out.println("Here is a merge conflict I create from local master");
 
 		System.out.println("here is the conflict we have from remote master branch...");
+		System.out.println("here is another conflict we have from local master branch...");
 
 
 	}
