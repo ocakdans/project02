ce67643721ad7000a07e45f736a8419b9dc15d47
ocakdans <selim.ocakdan@gmail.com>
2020-11-08 19:07:21 +0300

    second commit

--- src/firtstpackage/FirstClass.java
+++ src/firtstpackage/FirstClass.java
@@ -3,7 +3,8 @@ package firtstpackage;
 public class FirstClass {
 
 	public static void main(String[] args) {
-		System.out.println("first commit from local master branch");
+		System.out.println("first  commit from local master branch");
+		System.out.println("second commit from local master branch");
 
 	}
 
