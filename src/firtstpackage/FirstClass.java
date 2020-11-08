84cf62685428d17578f7f283f49387d5b94c72c7
ocakdans <73405885+ocakdans@users.noreply.github.com>
2020-11-08 20:12:20 +0300

    forth: here is the conflict

--- src/firtstpackage/FirstClass.java
+++ src/firtstpackage/FirstClass.java
@@ -9,6 +9,7 @@ public class FirstClass {
 		System.out.println("first  commit from remotemaster branch");
 		System.out.println("second commit from remotemaster branch");
 		System.out.println("third  commit from remotemaster branch");
+		System.out.println("here is the conflict we have from remote master branch...");
 
 	}
 
